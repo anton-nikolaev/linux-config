89ded91d7cda7b3ae718ec79bd2cc58699f19476
Anton G. Nikolaev <airay@yandex.ru>
2018-10-29 18:10:16 +0300

    win7 backup script updated with some cleanup prior

--- bin/win7backup.sh
+++ bin/win7backup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-/bin/dd if=/dev/sda3 bs=1M conv=noerror,sync status=progress > /home/anton/win7backup.dd
+DRIVE=/dev/sda3
+
+/bin/umount -v $DRIVE
+/bin/mount -v $DRIVE /root/mnt
+/bin/rm -rv /root/mnt/Windows/SoftwareDistribution/*
+/bin/rm -rv /root/mnt/Users/ANikolaev/AppData/Local/Temp/*
+/bin/umount -v $DRIVE
+/bin/dd if=$DRIVE bs=1M conv=noerror,sync status=progress > /home/anton/win7backup.dd
