06363dd53c248a04d0a810735acae8a1aa953b06
Anton G. Nikolaev <airay@yandex.ru>
2019-07-31 15:07:28 +0300

    added win7 du output parser

--- bin/win7backup.sh
+++ bin/win7backup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 DRIVE=/dev/sda3
+WIN7DUDIR=/home/anton/win7du
 
 /bin/umount -v $DRIVE
 /bin/mount -v $DRIVE /root/mnt
 /bin/rm -rv /root/mnt/Windows/SoftwareDistribution/*
 /bin/rm -rv /root/mnt/Users/ANikolaev/AppData/Local/Temp/*
-/bin/umount -v $DRIVE
-/bin/dd if=$DRIVE bs=1M conv=noerror,sync status=progress > /home/anton/win7backup.dd
+/usr/bin/du -h /root/mnt/ > $WIN7DUDIR/dump.`date +%Y%m%d%H%M%S`
+#/bin/umount -v $DRIVE
+#/bin/dd if=$DRIVE bs=1M conv=noerror,sync status=progress > /home/anton/win7backup.dd
---
+++ bin/win7duparse.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+
+use strict;
+use Data::Dumper;
+
+while (<>) {
+    next unless /^([\d\,KGM]+)\s+(.*)$/;
+    my $size = $1;
+    my $object = $2;
+    next if $size =~ /K$/; # drop smaller than 1M
+    next if $size == 0; # drop zero sized
+    $size =~ s/\,/./; # help perl understand this as a number
+    $size *= 1024 if $size =~ /G$/; # convert gigabytes to megabytes
+    $size =~ s/M$//; # chop off megabytes char
+    next unless $size > 100; # drop smaller than 100M
+    print STDOUT "$size $object\n";
+}
