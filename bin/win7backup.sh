78bb6fd4dddb04fe81f10f9ff5f11e50d73c71bc
Anton G. Nikolaev <airay@yandex.ru>
2019-10-04 15:37:47 +0300

    du parse script fixed: small object filtered

--- bin/win7backup.sh
+++ bin/win7backup.sh
@@ -8,5 +8,5 @@ WIN7DUDIR=/home/anton/win7du
 /bin/rm -rv /root/mnt/Windows/SoftwareDistribution/*
 /bin/rm -rv /root/mnt/Users/ANikolaev/AppData/Local/Temp/*
 /usr/bin/du -h /root/mnt/ > $WIN7DUDIR/dump.`date +%Y%m%d%H%M%S`
-#/bin/umount -v $DRIVE
-#/bin/dd if=$DRIVE bs=1M conv=noerror,sync status=progress > /home/anton/win7backup.dd
+/bin/umount -v $DRIVE
+/bin/dd if=$DRIVE bs=1M conv=noerror,sync status=progress > /home/anton/win7backup.dd
--- bin/win7duparse.pl
+++ bin/win7duparse.pl
@@ -7,7 +7,9 @@ while (<>) {
     next unless /^([\d\,KGM]+)\s+(.*)$/;
     my $size = $1;
     my $object = $2;
+    next unless $size =~ /\w$/;
     next if $size =~ /K$/; # drop smaller than 1M
+    next if $size =~ /^\d+$/; # drop smaller than 1K
     next if $size == 0; # drop zero sized
     $size =~ s/\,/./; # help perl understand this as a number
     $size *= 1024 if $size =~ /G$/; # convert gigabytes to megabytes
